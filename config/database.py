7bfaa58dbcb759440d83c84f400d001ca63588a0
Hepik <hepiky@gmail.com>
2025-01-11 15:46:33 +0100

    automation: fix postgres dependencies

--- config/database.py
+++ config/database.py
@@ -7,9 +7,7 @@ import os
 load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.environ["SQLALCHEMY_DATABASE_URL"]
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
